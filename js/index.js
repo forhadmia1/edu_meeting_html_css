62af17f083ce498bfd2a84300701917e92701829
forhadmia1 <forhad8613@gmail.com>
2022-10-04 23:12:50 +0600

    accordion implement done

--- js/index.js
+++ js/index.js
@@ -17,4 +17,33 @@ document.getElementById('body').addEventListener('click', () => {
     menuContainer.style.display = 'none'
 })
 
+// accordion functionality
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+const defaultAccordion = accordionItemHeaders[0]
+defaultAccordion.classList.add("active");
+defaultAccordion.nextElementSibling.style.maxHeight = '200px';
 
+
+const accordion = () => {
+    accordionItemHeaders.forEach(accordionItemHeader => {
+        accordionItemHeader.classList.remove("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        accordionItemBody.style.maxHeight = 0;
+    })
+}
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+        accordion()
+        accordionItemHeader.classList.add("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+
+    });
+});
+// accordion functionality
